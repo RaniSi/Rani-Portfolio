8a21c18beb6ab65581447294b9709ffb346e22aa
Rani Sipani <ranisipani879@gmail.com>
2025-07-30 23:11:04 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,10 +8,10 @@ export const HeroSection = () => {
                 <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
                     <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
                     <span className="text-primary opacity-0 animate-fade-in-delay-1">
-                        {' '}Saman
+                        {' '}Rani
                     </span>
                     <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-1">
-                        {' '}Raj
+                        {' '}Sipani
                     </span>
                 </h1>
                 <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
