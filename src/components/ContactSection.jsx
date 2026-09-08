3462b4a74d7de7e35254eed494732c281f33d696
Rani Sipani <ranisipani879@gmail.com>
2026-09-08 10:11:32 +0530

    Address Updation

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -69,7 +69,7 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-medium"> Location</h4>
                   <a className="text-muted-foreground hover:text-primary transition-colors">
-                    Bikaner, Rajasthan, India
+                    Rajasthan, India
                   </a>
                 </div>
               </div>
